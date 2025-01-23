ba6399c72a48823c6f5e8bd4d2f7b9c26ee6b2e6
fredy <fredy-andri.mottus@voco.ee>
2025-01-23 15:46:05 +0200

    yl 8 ymbritseva komponendi rakendamine

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
 import './ExpenseItem.css'
+import Card from './Card.jsx'
 import ExpenseDate from "./ExpenseDate.jsx";
 
 function ExpenseItem(props){
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.data.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">{props.data.price}€</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
