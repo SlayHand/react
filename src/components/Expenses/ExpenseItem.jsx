0ff1d2dbe9688d5a08da851fe33d169e85914c18
fredy <fredy-andri.mottus@voco.ee>
2025-01-24 12:24:02 +0200

    ylesanne 10 syndmuste tootlus komponentides

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,8 +1,18 @@
 import './ExpenseItem.css'
 import Card from '../UI/Card.jsx'
-import ExpenseDate from "./ExpenseDate.jsx";
+import ExpenseDate from "./ExpenseDate.jsx"
+import { useState } from 'react'
+
+
 
 function ExpenseItem(props){
+    const [title, setTitle] = useState(props.data.title)
+    
+    const clickHandler = () => {
+        console.log('Clicked!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+    }
 
     return (
         <Card className="expense-item">
@@ -11,6 +21,7 @@ function ExpenseItem(props){
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">{props.data.price}€</div>
             </div>
+            <button onClick={clickHandler}>Click Me</button>
         </Card>
     )
 }
