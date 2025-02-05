26fd79b2c6c698f6ce4887bafb11f8d5bf63489b
fredy <fredy-andri.mottus@voco.ee>
2025-02-05 12:50:56 +0200

    12. Ülesanne 4 Lahendatud

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import ExpenseItem from './ExpenseItem.jsx'
 import Card from '../UI/Card.jsx'
 import './Expenses.css'
-
+import ExpensesFilter from './ExpensesFilter.jsx'
 
 
 
 const Expenses = (props) => {
 
+  const [filteredYear, setFilteredYear] = useState('2024');
+
+  const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log(`Your data in Expenses.jsx ${selectedYear}`);
+      }
   return (
     <Card className='expenses'>
+        <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} />
         <ExpenseItem data={props.expenses[0]}/>
         <ExpenseItem data={props.expenses[1]}/>
     </Card>
